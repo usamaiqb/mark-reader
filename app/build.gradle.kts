448aa56991ed7277f34cf71091508ca8e479657a
Usama <83345144+usamaiqb@users.noreply.github.com>
2026-09-12 00:12:41 +0500

    chore(release): v1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.markreader"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
